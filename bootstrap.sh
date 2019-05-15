2182c16107876b759991a02c379293a8b019d123
Manuel Hutter <manuel@hutter.io>
2019-05-15 12:25:31 +0200

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+function kapply {
+    kubectl "$@" --save-config --dry-run --output yaml | kubectl apply -f -
+}
+
+export TILLER_NAMESPACE=kube-system
+export FLUX_FORWARD_NAMESPACE=flux
+export GIT_REPO='git@github.com:mhutter/flux-hnetes.git'
+
+kapply create namespace "$TILLER_NAMESPACE"
+kapply create clusterrolebinding tiller-admin --clusterrole cluster-admin --serviceaccount "$TILLER_NAMESPACE:default"
+
+helm init \
+  --upgrade \
+  --tiller-namespace $TILLER_NAMESPACE \
+  --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' \
+  --history-max 10 \
+  --wait
+
+helm repo add weaveworks https://weaveworks.github.io/flux
+helm repo update
+
+kubectl apply -f https://raw.githubusercontent.com/weaveworks/flux/master/deploy-helm/flux-helm-release-crd.yaml
+helm upgrade --install flux weaveworks/flux \
+  --set helmOperator.create=true \
+  --set helmOperator.createCRD=false \
+  --set helmOperator.tillerNamespace=$TILLER_NAMESPACE \
+  --set git.url=$GIT_REPO \
+  --namespace $FLUX_FORWARD_NAMESPACE \
+  --wait
+
+if hash fluxctl 2>/dev/null; then
+    fluxctl identity
+fi
