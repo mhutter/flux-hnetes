c00ec270be9249a8e9b756fe6e4a322693c8510b
Manuel Hutter <manuel@hutter.io>
2019-05-24 17:24:57 +0200

    Deploy tiller to the "helm" namespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail
 
-function kapply {
-    kubectl "$@" --save-config --dry-run --output yaml | kubectl apply -f -
-}
-
-export TILLER_NAMESPACE=kube-system
+export TILLER_NAMESPACE=helm
 export FLUX_FORWARD_NAMESPACE=flux
-export GIT_REPO='git@github.com:mhutter/flux-hnetes.git'
+GIT_REPO='git@github.com:mhutter/flux-hnetes.git'
 
-kapply create namespace "$TILLER_NAMESPACE"
-kapply create clusterrolebinding tiller-admin --clusterrole cluster-admin --serviceaccount "$TILLER_NAMESPACE:default"
-
+#
+# Deploy Helm
+#
+kubectl apply -f ./helm/
 helm init \
   --upgrade \
-  --tiller-namespace $TILLER_NAMESPACE \
+  --tiller-namespace "$TILLER_NAMESPACE" \
+  --service-account tiller \
   --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' \
   --history-max 10 \
   --wait
 
 helm repo add weaveworks https://weaveworks.github.io/flux
 helm repo update
-
 helm install weaveworks/flux \
   --name flux \
   --set helmOperator.create=true \
